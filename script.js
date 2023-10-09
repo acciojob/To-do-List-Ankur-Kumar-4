5f2f884c5ff04f4ef3d623825f5c555bfafc732d
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-10-09 11:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.getElementById("addTodoBtn").addEventListener("click",()=>{
 	let value = document.getElementById("newTodoInput").value;
 	let addlist = document.getElementById("todoList");
 	let list = document.createElement("li");
+	if(value!=""){
 	list.innerHTML=value;
 	addlist.appendChild(list);
+	}
 });
 
