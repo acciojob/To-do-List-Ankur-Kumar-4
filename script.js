ae2aa77079ecb98d71805a139aee2f4b9b1cbfc8
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-10-09 11:25:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-//your code here
+document.getElementById("addTodoBtn").addEventListener("click",()=>{
+	let value = document.getElementById("newTodoInput").value;
+	let addlist = document.getElementById("todoList");
+	let list = document.createElement("li");
+	list.innerHTML=value;
+	addlist.appendChild(list);
+});
 
